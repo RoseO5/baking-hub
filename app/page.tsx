4b8f5d8793f51f248e2d694fd057c90d1fc1af92
RoseO5 <talktorose90@gmail.com>
2026-04-23 15:56:01 +0100

    Fix form JSON headers

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,7 @@ export default function Home() {
 
           await fetch("/api/questions", {
             method: "POST",
+      headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ question }),
           });
 
